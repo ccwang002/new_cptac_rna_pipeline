3805f4b8abbe2c97f7b9830f1a4783691faef12b
Liang-Bo Wang <liang-bo.wang@wustl.edu>
2020-07-17 17:44:00 -0500

    Fix snakefile name

--- example_batch/run_snakemake_main_job.sh
+++ example_batch/run_snakemake_main_job.sh
@@ -3,7 +3,7 @@
 snakemake \
     --profile=/storage1/fs1/dinglab/Active/Projects/CPTAC3/Analysis/bulk_rna_pipeline_development/new_cptac_rna_pipeline/compute1_lsf \
     --configfile=snakemake_config.json \
-    -s /storage1/fs1/dinglab/Active/Projects/CPTAC3/Analysis/bulk_rna_pipeline_development/new_cptac_rna_pipeline/star.smk \
+    -s /storage1/fs1/dinglab/Active/Projects/CPTAC3/Analysis/bulk_rna_pipeline_development/new_cptac_rna_pipeline/rnaseq.smk \
     -j 10 \
     -d $PWD \
     --restart-times 2 \
